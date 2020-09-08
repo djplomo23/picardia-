25e88118094ab46610ce60b57a1b9d77aacc4e20
djplomo23 <69323758+djplomo23@users.noreply.github.com>
2020-09-08 19:08:56 -0400

    Updates

---
+++ precache-manifest.5a0f262af581bd0672a11be54a978513.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b2bb1a7eb91ec6972181fe651f447c",
+    "url": "/picardia-/index.html"
+  },
+  {
+    "revision": "04ec7deb9e2f4c109db3",
+    "url": "/picardia-/static/css/main.3845b4ab.chunk.css"
+  },
+  {
+    "revision": "1f24573e6d7fd735d964",
+    "url": "/picardia-/static/js/2.e835b665.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/picardia-/static/js/2.e835b665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ec7deb9e2f4c109db3",
+    "url": "/picardia-/static/js/main.c9433505.chunk.js"
+  },
+  {
+    "revision": "f9815453ead63b5dc369",
+    "url": "/picardia-/static/js/runtime-main.8cfa025f.js"
+  },
+  {
+    "revision": "c782774f2084ca9e10d9847140619a05",
+    "url": "/picardia-/static/media/LOGO-PICARDIA--BLANCO.c782774f.png"
+  },
+  {
+    "revision": "d4fd60af5a1836ef835fe9cf10d3049b",
+    "url": "/picardia-/static/media/LOGO-PICARDIA-NEGRO.d4fd60af.png"
+  },
+  {
+    "revision": "0446df373cc7501fa9397232f855addf",
+    "url": "/picardia-/static/media/LOGO-PICARDIA-Silueta.0446df37.png"
+  },
+  {
+    "revision": "cb5602501f63710e6e479943a39d0a3f",
+    "url": "/picardia-/static/media/Offer.cb560250.png"
+  },
+  {
+    "revision": "1735c3c6aa36108634f0414f6a5e27a6",
+    "url": "/picardia-/static/media/Offer1.1735c3c6.png"
+  },
+  {
+    "revision": "da0a133b40ea9cf1c89d4841362ebff8",
+    "url": "/picardia-/static/media/esposas.da0a133b.png"
+  },
+  {
+    "revision": "61bd2cdfdaea2d83eedfcafa31e637e5",
+    "url": "/picardia-/static/media/rostro.61bd2cdf.jpg"
+  }
+]);
